28a0b5817cde641a4d9b729c559c0393d4529765
Marian <marianlopig@gmail.com>
2022-05-22 15:27:30 +0200

    adding all fields for user and config the picture upload

--- src/database/models/User.js
+++ src/database/models/User.js
@@ -3,6 +3,8 @@ const { Schema, model } = require("mongoose");
 const UserSchema = new Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  name: { type: String },
+  image: { type: String },
 });
 
 const User = model("User", UserSchema, "users");
